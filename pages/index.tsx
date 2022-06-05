d6c09235a9d083cce8b8614bd9fa2cc97d14c27c
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-06-05 18:23:25 +0200

    -added: profile link

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ const Home: NextPage = () => {
       <span className='mx-4 text-cyan-700'>Hello World</span>
       <a href="/api/auth/login" className='mx-4 text-fuchsia-900'>Login</a>
       <a href="/api/auth/logout" className='mx-4 text-green-900'>Logout</a>
+      <a href="/profile" className='mx-4 text-orange-900'>Profile</a>
     </div>
   )
 }
