d3c9822a1d068c27ade7e151b420cafc1edb51b3
Thomas Zerr <thomas.zerr.3@hof-university.de>
2022-06-05 17:51:24 +0200

    -added: profile functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from 'next'
 const Home: NextPage = () => {
   return (
     <div className='container'>
-      <span className='text-cyan-700'>Hello World</span>
+      <span className='mx-4 text-cyan-700'>Hello World</span>
+      <a href="/api/auth/login" className='mx-4 text-fuchsia-900'>Login</a>
+      <a href="/api/auth/logout" className='mx-4 text-green-900'>Logout</a>
     </div>
   )
 }
---
+++ pages/profile/index.tsx
@@ -0,0 +1,24 @@
+import { useUser } from "@auth0/nextjs-auth0";
+import Image from "next/image";
+
+const Profile = () => {
+  const {user, error, isLoading} = useUser();
+
+  if (user?.picture === null) user.picture = undefined;
+  if (user?.name === null) user.name = undefined;
+
+  if (isLoading) return <div>Loading...</div>
+  if (error) return <div>{error.message}</div>
+
+  return (
+    user && (
+      <div>
+        {user.picture && <Image src={user.picture} alt={user.name} width={32} height={32}/>}
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  )
+}
+
+export default Profile;
